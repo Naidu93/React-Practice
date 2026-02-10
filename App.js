d87678c355e650225298052600db62d7ec86e30a
Naidu93 <brahmanaidum93@gmail.com>
2026-02-10 09:13:47 +0530

    Day 02

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const heading = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child-1"},React.createElement("h1",{},"This is H1 from Child1")),
-    React.createElement("div",{id:"child-2"},React.createElement("h1",{},"This is H1 from Child2")),
+    React.createElement("div",{id:"child-1",key:"p1"},React.createElement("h1",{key:"1"},"This is H1 from Child1")),
+    React.createElement("div",{id:"child-2",key:"p2"},React.createElement("h1",{key:"2"},"This is H1 from Child2")),
 ])
 
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React 🚀
+
+# Parcel
+    - Dev Builds
+    - Local Server
+    - HMR - Hot Module Replacement (Refreshing)
+    - File Watching Algorithm - written in c++
+    - Caching - Faster Builds
+    - Image Optimization
+    - Minification
+    - Bundling
+    - Compress
+    - Consistent Hashing
+    - Code Splitting
+    - Differential Bundling - supports older browsers
+    - Diagnostic
+    - Error Handling
+    - HTTPs
+    - Tree Shaking - remove unused code
+    - Different dev and prod bundles
+
+    
